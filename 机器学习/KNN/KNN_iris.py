59faa22faa738daa64cb7ad254222c47ec125181
CYF <zjw@zjw.com>
2025-04-23 21:52:19 +0800

    手写数字识别KNN算法

--- 机器学习/KNN/KNN_iris.py
+++ 机器学习/KNN/KNN_iris.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
 #加载数据
 iris_data=load_iris()
 data=pd.DataFrame(iris_data.data,columns=iris_data.feature_names)
@@ -23,9 +24,16 @@ ss=StandardScaler()
 x_train=ss.fit_transform(x_train)
 x_test=ss.transform(x_test)
 #模型训练
-model=KNeighborsClassifier(n_neighbors=3)
-model.fit(x_train,y_train)
+model=KNeighborsClassifier(n_neighbors=1)
+#交叉验证网络搜索,找出最合适的超参数
+paras_grid={'n_neighbors':[1,3,5,7,9]}
+estimator=GridSearchCV(model,paras_grid,cv=4)
+estimator.fit(x_train,y_train)
 #模型评估
-y_pred=model.predict(x_test)
+y_pred=estimator.predict(x_test)
 print(accuracy_score(y_test,y_pred))
+print(estimator.best_score_)
+print(estimator.best_params_)
+print(estimator.best_estimator_)
+print(estimator.cv_results_)
 #模型测试
